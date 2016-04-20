3706f02ce6b15dbaa4342e1fd084be28e0699dd1
Adam Liu <adam.liu84@gmail.com>
2016-04-20 23:37:22 +0800

    Draft code for listing image files && load of picturebox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxLoadedDirectory = new System.Windows.Forms.TextBox();
+            this.buttonLoadDirImage = new System.Windows.Forms.Button();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxLoadedDirectory
+            // 
+            this.textBoxLoadedDirectory.Location = new System.Drawing.Point(7, 8);
+            this.textBoxLoadedDirectory.Name = "textBoxLoadedDirectory";
+            this.textBoxLoadedDirectory.Size = new System.Drawing.Size(516, 20);
+            this.textBoxLoadedDirectory.TabIndex = 0;
+            this.textBoxLoadedDirectory.Text = "P:\\\\ImageShifterSample";
+            // 
+            // buttonLoadDirImage
+            // 
+            this.buttonLoadDirImage.Location = new System.Drawing.Point(539, 6);
+            this.buttonLoadDirImage.Name = "buttonLoadDirImage";
+            this.buttonLoadDirImage.Size = new System.Drawing.Size(145, 23);
+            this.buttonLoadDirImage.TabIndex = 1;
+            this.buttonLoadDirImage.Text = "Load Directory Image";
+            this.buttonLoadDirImage.UseVisualStyleBackColor = true;
+            this.buttonLoadDirImage.Click += new System.EventHandler(this.buttonLoadDirImage_Click);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxPreview.Location = new System.Drawing.Point(7, 34);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(516, 385);
+            this.pictureBoxPreview.TabIndex = 2;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 466);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.buttonLoadDirImage);
+            this.Controls.Add(this.textBoxLoadedDirectory);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxLoadedDirectory;
+        private System.Windows.Forms.Button buttonLoadDirImage;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,22 @@ namespace ImageShifter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLoadDirImage_Click(object sender, EventArgs e)
+        {
+            //Primitive of searching image files of given directory 
+            String szPath = textBoxLoadedDirectory.Text;
+            String[] aszFiles = Directory.GetFiles(@szPath);
+            String[] aszImageFileExt = { "jpg", "exe", "gif" };
+            IEnumerable<String> aszImageFiles = aszFiles.Where(f => aszImageFileExt.Contains(f.Split('.')[1]));
+            foreach (String szCurFile in aszImageFiles)
+            {
+                Console.WriteLine(szCurFile);             
+            }
+
+            //Testing loading of 1 image //ignore failure for now
+            pictureBoxPreview.Image = Image.FromFile(aszImageFiles.First());
         }
     }
 }
